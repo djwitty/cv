324289b8df812579837df86c5a564eb923d05efe
Michael aka DjWitty <michael.pov.it@gmail.com>
2019-10-10 15:27:47 +0300

    modified:   build/asset-manifest.json 	modified:   build/index.html 	modified:   build/service-worker.js 	deleted:    build/static/js/main.b265c1c3.js 	deleted:    build/static/js/main.b265c1c3.js.map 	new file:   build/static/js/main.dfbd656b.js 	new file:   build/static/js/main.dfbd656b.js.map 	modified:   package.json 	modified:   src/App.js 	modified:   src/components/landingpage.js 	modified:   src/components/main.js 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -9,25 +9,24 @@ class App extends Component {
     return (
       <div className="demo-big-content">
     <Layout>
-        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">My Web Site</Link>} scroll>
-            <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/contact">Contact me</Link>
-            </Navigation>
-        </Header>
-        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">My Web Site</Link>}>
-            <Navigation>
+        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/cv">My Web Site</Link>} scroll>
+          <Navigation>
               <Link to="/resume">Resume</Link>
               <Link to="/contact">Contact me</Link>
-            </Navigation>
+          </Navigation>
+        </Header>
+        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">My Web Site</Link>}>
+          <Navigation>
+            <Link to="/resume">Resume</Link>
+            <Link to="/contact">Contact me</Link>
+          </Navigation>
         </Drawer>
         <Content>
             <div className="page-content" />
             <Main/>
         </Content>
     </Layout>
-</div>
-
+      </div>
     );
   }
 }
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
 
-
 class Landing extends Component {
   render() {
     return(
@@ -9,41 +8,32 @@ class Landing extends Component {
         <Grid className="landing-grid">
           <Cell col={12}>
             <img
-              src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+              src="https://web-dev-studio.com/ava.jpg"
               alt="avatar"
               className="avatar-img"
               />
-
+              <h1>Project Manager | Web-developer</h1>
             <div className="banner-text">
-              <h1>Project Manager</h1>
-
-            <hr/>
-
-          <p>HTML/CSS | Bootstrap | JavaScript </p>
-
-        <div className="social-links">
-
-          {/* LinkedIn */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-linkedin-square" aria-hidden="true" />
-          </a>
-
-          {/* Github */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-github-square" aria-hidden="true" />
-          </a>
-
-          {/* Freecodecamp */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-free-code-camp" aria-hidden="true" />
-          </a>
-
-          {/* Youtube */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-youtube-square" aria-hidden="true" />
-          </a>
-
-        </div>
+              <h2 style={{align: 'center', color: 'white', margin: '0'}}>Hard Skills</h2>
+              <p style={{margin: '0'}}>
+                <b>PM: </b>Agile(SCRUM/Kanban), Waterfall, Jira/Confluence/Trello
+              </p>
+              <p>
+                <b>Web-Dev: </b>HTML/CSS/JS | Gulp/Webpack/Bootstrap/Git | Linux user&nbsp;&nbsp;&nbsp;&nbsp;<b>CMS: </b>WP/OC/Magento etc.
+              </p>
+              <hr/>
+              <h2 style={{align: 'center', color: 'white', margin: '0'}}>Soft Skills</h2>
+              <p>Time management | Responsibility | Team leadership | Customer Support | Communicative | Reliable</p>
+              <div className="social-links">
+                {/* LinkedIn */}
+                <a href="https://www.linkedin.com/in/witty87/" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
+                {/* Github */}
+                <a href="https://github.com/djwitty" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
+              </div>
             </div>
           </Cell>
         </Grid>
